13bd8861277b30690fb11bdb6b3d1cdb9a337784
H0rn0chse <h0rn0chse@gmx.de>
2022-02-06 17:24:00 +0100

    Moved client

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 import { startServer } from "@h0rn0chse/socket-server";
 
 startServer({
-    publicPaths: [["/", "/"]]
+    publicPaths: [[
+        "/client", "/"
+    ], [
+        "/node_modules/bootstrap/dist", "/bootstrap"
+    ], [
+        "/node_modules/bootstrap-vue/dist", "/bootstrap-vue"
+    ], [
+        "/node_modules/vue-custom-scrollbar/dist", "/vue-custom-scrollbar"
+    ], [
+        "/node_modules/vue/dist", "/vue"
+    ], [
+        "/node_modules/vuex/dist", "/vuex"
+    ], [
+        "/node_modules/feather-icons/dist", "/feather-icons"
+    ], [
+        "/node_modules/vue-markdown/dist", "/vue-markdown"
+    ]]
 });
