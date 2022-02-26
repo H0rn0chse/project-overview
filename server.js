969592e4261c9e8d21a198c916f047a1c177d8ca
H0rn0chse <h0rn0chse@gmx.de>
2022-02-26 20:54:41 +0100

    Updating workflow and adding anonymized statistics

--- server.js
+++ server.js
@@ -1,25 +1,9 @@
 import { startServer } from "@h0rn0chse/socket-server";
+import fs from "fs";
+
+const json = fs.readFileSync("./gh-pages-dependencies.json");
+const dependencies = JSON.parse(json);
 
 startServer({
-    publicPaths: [[
-        "/client", "/"
-    ], [
-        "/node_modules/bootstrap/dist", "/libs/bootstrap"
-    ], [
-        "/node_modules/bootstrap-vue/dist", "/libs/bootstrap-vue"
-    ], [
-        "/node_modules/vue-custom-scrollbar/dist", "/libs/vue-custom-scrollbar"
-    ], [
-        "/node_modules/vue/dist", "/libs/vue"
-    ], [
-        "/node_modules/vuex/dist", "/libs/vuex"
-    ], [
-        "/node_modules/feather-icons/dist", "/libs/feather-icons"
-    ], [
-        "/node_modules/@adapttive/vue-markdown/dist", "/libs/vue-markdown"
-    ], [
-        "/node_modules/simple-icons", "/libs/simple-icons"
-    ], [
-        "/node_modules/wc-github-corners/dist", "/libs/wc-github-corners"
-    ]]
+    publicPaths: dependencies
 });
